14e1f869fd9d11d882fc3bb6836371080ca542cd
kchiranjewee63 <kchiranjewee63@gmail.com>
2024-07-31 16:04:12 -0400

    Support zap logger level configuration through environment variable

--- service/cmd/main.go
+++ service/cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -12,6 +11,7 @@ import (
 	"github.com/tratteria/tratteria-agent/api"
 	"github.com/tratteria/tratteria-agent/config"
 	"github.com/tratteria/tratteria-agent/configsync"
+	"github.com/tratteria/tratteria-agent/logging"
 	"github.com/tratteria/tratteria-agent/tratinterceptor"
 	"github.com/tratteria/tratteria-agent/tratteriatrustbundlemanager"
 	"github.com/tratteria/tratteria-agent/tratverifier"
@@ -27,16 +27,12 @@ func main() {
 
 	setupSignalHandler(cancel)
 
-	logger, err := zap.NewProduction()
-	if err != nil {
-		log.Fatalf("Cannot initialize Zap logger: %v.", err)
+	if err := logging.InitLogger(); err != nil {
+		panic(err)
 	}
+	defer logging.Sync()
 
-	defer func() {
-		if err := logger.Sync(); err != nil {
-			log.Printf("Error syncing logger: %v", err)
-		}
-	}()
+	logger := logging.GetLogger("main")
 
 	x509SrcCtx, cancel := context.WithTimeout(context.Background(), X509_SOURCE_TIMEOUT)
 	defer cancel()
@@ -51,7 +47,7 @@ func main() {
 	appConfig := config.GetAppConfig()
 
 	verificationRules := v1alpha1.NewVerificationRulesImp()
-	configSyncClient := configsync.NewClient(appConfig.TconfigdHost, appConfig.TconfigdSpiffeID, appConfig.MyNamespace, verificationRules, x509Source, logger)
+	configSyncClient := configsync.NewClient(appConfig.TconfigdHost, appConfig.TconfigdSpiffeID, appConfig.MyNamespace, verificationRules, x509Source, logging.GetLogger("config-sync"))
 
 	go func() {
 		if err := configSyncClient.Start(ctx); err != nil {
@@ -68,7 +64,7 @@ func main() {
 			ApiPort:                  appConfig.AgentHttpApiPort,
 			VerificationRulesManager: verificationRules,
 			TraTVerifier:             tratVerifier,
-			Logger:                   logger}
+			Logger:                   logging.GetLogger("api-server")}
 
 		if err := apiServer.Run(); err != nil {
 			logger.Fatal("Failed to start HTTP server.", zap.Error(err))
@@ -81,7 +77,7 @@ func main() {
 		}
 
 		go func() {
-			tratInterceptor, err := tratinterceptor.NewTraTInterceptor(*appConfig.ServicePort, appConfig.AgentInterceptorPort, tratVerifier, logger)
+			tratInterceptor, err := tratinterceptor.NewTraTInterceptor(*appConfig.ServicePort, appConfig.AgentInterceptorPort, tratVerifier, logging.GetLogger("trat-interceptor"))
 			if err != nil {
 				logger.Fatal("Failed to start tratinterceptor.", zap.Error(err))
 			}
